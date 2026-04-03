f153d5db15c2f8b82a8b7612ba9555ed24f66f7c
Carlos Bergueira <carlos.bergueira@gmail.com>
2026-04-03 22:56:17 +0100

    apagar a pasta _CB

--- Python/_CB/agents/base_agent.py
+++
@@ -1,9 +0,0 @@
-class BaseAgent:
-    def __init__(self, name):
-        self.name = name
-
-    def log(self, msg):
-        print(f"[{self.name}] {msg}")
-
-    def handle(self, data):
-        raise NotImplementedError
